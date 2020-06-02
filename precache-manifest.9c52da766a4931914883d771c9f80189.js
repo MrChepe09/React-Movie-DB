0ba334db7b89199bdb9bdd06dc2f3dda8e500ffa
Bhupinder Singh <khanujabhupinder09@gmail.com>
2020-06-02 18:58:52 +0530

    changes

---
+++ precache-manifest.9c52da766a4931914883d771c9f80189.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3cdc65c97290ae81079bbe8523f4aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f769c45004952f07ef89",
+    "url": "/static/css/main.585915a3.chunk.css"
+  },
+  {
+    "revision": "31a0d0cf5ea78751d5be",
+    "url": "/static/js/2.badbdaf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.badbdaf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f769c45004952f07ef89",
+    "url": "/static/js/main.a482aedd.chunk.js"
+  },
+  {
+    "revision": "2b4100ddc1326b89db2e",
+    "url": "/static/js/runtime-main.cb8448f4.js"
+  }
+]);
